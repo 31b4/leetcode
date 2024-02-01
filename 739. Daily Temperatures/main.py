89c4d12334b31ad511971ab6208f7518503e1441
ben <75566095+31b4@users.noreply.github.com>
2024-02-01 13:32:59 +0100

    Create main.py

---
+++ 739. Daily Temperatures/main.py
@@ -0,0 +1,13 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        answer = [0] * n
+        stack = []
+        
+        for curr_day, curr_temp in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < curr_temp:
+                prev_day = stack.pop()
+                answer[prev_day] = curr_day - prev_day
+            stack.append(curr_day)
+        
+        return answer
