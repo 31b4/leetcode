6cc75b2cdf1f4308a5f4747ce76defac6cbfae10
ben <75566095+31b4@users.noreply.github.com>
2024-02-02 22:56:42 +0100

    Create main.py

---
+++ 1291. Sequential Digits/main.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sequentialDigits(self, low, high):
+        a = []
+
+        for i in range(1, 10):
+            num = i
+            next_digit = i + 1
+
+            while num <= high and next_digit <= 9:
+                num = num * 10 + next_digit
+                if low <= num <= high:
+                    a.append(num)
+                next_digit += 1
+
+        a.sort()
+        return a
+
