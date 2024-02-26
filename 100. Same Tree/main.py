dad1c226bdb35b53457db369a3ee4ce8977b7806
ben <75566095+31b4@users.noreply.github.com>
2024-02-26 15:35:36 +0100

    Create main.py

---
+++ 100. Same Tree/main.py
@@ -0,0 +1,8 @@
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
