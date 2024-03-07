1ed00d4766582956689d3ad18a0b46991aea30c8
ben <75566095+31b4@users.noreply.github.com>
2024-03-07 17:35:23 +0100

    Create main.py

---
+++ 876. Middle of the Linked List/main.py
@@ -0,0 +1,10 @@
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow_pointer = head
+        fast_pointer = head
+
+        while fast_pointer is not None and fast_pointer.next is not None:
+            slow_pointer = slow_pointer.next
+            fast_pointer = fast_pointer.next.next
+
+        return slow_pointer
