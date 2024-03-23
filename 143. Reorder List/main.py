fe91ce3b51c8de0c99e891d550df49d438fe7932
ben <75566095+31b4@users.noreply.github.com>
2024-03-23 01:04:10 +0100

    Create main.py

---
+++ 143. Reorder List/main.py
@@ -0,0 +1,25 @@
+class Solution:
+    def reorderList(self, head):
+        #step 1: find middle
+        if not head: return []
+        slow, fast = head, head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        #step 2: reverse second half
+        prev, curr = None, slow.next
+        while curr:
+            nextt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextt    
+        slow.next = None
+        
+        #step 3: merge lists
+        head1, head2 = head, prev
+        while head2:
+            nextt = head1.next
+            head1.next = head2
+            head1 = head2
+            head2 = nextt
