b66cc28a65b3a9ebc37e90837726909a6080109b
ben <75566095+31b4@users.noreply.github.com>
2024-03-31 23:29:14 +0200

    Create main.py

---
+++ 2444. Count Subarrays With Fixed Bounds/main.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countSubarrays(self, nums: List[int], mink: int, maxK: int) -> int:
+
+        res = 0
+        bad_idx = left_idx = right_idx = -1
+
+        for i, num in enumerate(nums) :
+            if not mink <= num <= maxK:
+                bad_idx = i
+
+            if num == mink:
+                left_idx = i
+
+            if num == maxK:
+                right_idx = i
+
+            res += max(0, min(left_idx, right_idx) - bad_idx)
+
+        return res
