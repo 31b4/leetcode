2c6ba77dec0d637b4b699c9d697f481ae9552712
ben <75566095+31b4@users.noreply.github.com>
2024-07-30 16:14:24 +0200

    Create main.py

---
+++ 1653. Minimum Deletions to Make String Balanced/main.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimumDeletions(self, s: str) -> int:
+        n = len(s)
+        f = [0] * (n + 1)
+        b = 0
+        for i, c in enumerate(s, 1):
+            if c == 'b':
+                f[i] = f[i - 1]
+                b += 1
+            else:
+                f[i] = min(f[i - 1] + 1, b)
+        return f[n]
