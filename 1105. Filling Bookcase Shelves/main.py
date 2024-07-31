da93f8eab9488300c38d11d2f9651f809dec04e5
ben <75566095+31b4@users.noreply.github.com>
2024-07-31 15:49:52 +0200

    Create main.py

---
+++ 1105. Filling Bookcase Shelves/main.py
@@ -0,0 +1,25 @@
+class Solution:
+
+    def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
+
+        n = len(books)
+
+        f = [0] * (n + 1)
+
+        for i, (w, h) in enumerate(books, 1):
+
+            f[i] = f[i - 1] + h
+
+            for j in range(i - 1, 0, -1):
+
+                w += books[j - 1][0]
+
+                if w > shelfWidth:
+
+                    break
+
+                h = max(h, books[j - 1][1])
+
+                f[i] = min(f[i], f[j - 1] + h)
+
+        return f[n]    
