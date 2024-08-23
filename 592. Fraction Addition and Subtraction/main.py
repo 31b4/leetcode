aa453bdfe9e19928703a2d53bc9360eb2476a3a6
ben <75566095+31b4@users.noreply.github.com>
2024-08-23 22:24:17 +0200

    Create main.py

---
+++ 592. Fraction Addition and Subtraction/main.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fractionAddition(self, expression: str) -> str:
+        nums = list(map(int, re.findall(r'[+-]?\d+', expression)))
+        numerator = 0
+        denominator = 1
+        
+        for i in range(0, len(nums), 2):
+            num, den = nums[i], nums[i + 1]
+            numerator = numerator * den + num * denominator
+            denominator *= den
+        
+        common_divisor = gcd(numerator, denominator)
+        return f"{numerator // common_divisor}/{denominator // common_divisor}"
