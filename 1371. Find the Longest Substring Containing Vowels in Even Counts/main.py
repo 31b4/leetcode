29924fea9769efb01b453e59523a09ea0dfc065b
ben <75566095+31b4@users.noreply.github.com>
2024-09-15 12:18:39 +0200

    Create main.py

---
+++ 1371. Find the Longest Substring Containing Vowels in Even Counts/main.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findTheLongestSubstring(self, s: str) -> int:
+        mapy = [-2] * 32
+        mapy[0] = -1
+
+        max_len = 0
+        mask = 0
+
+        for i, char in enumerate(s):
+            if char == 'a':
+                mask ^= 1
+            elif char == 'e':
+                mask ^= 2
+            elif char == 'i':
+                mask ^= 4
+            elif char == 'o':
+                mask ^= 8
+            elif char == 'u':
+                mask ^= 16
+
+            prev = mapy[mask]
+            if prev == -2:
+                mapy[mask] = i
+            else:
+                max_len = max(max_len, i - prev)
+
+        return max_len
