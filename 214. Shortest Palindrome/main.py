9af5c266d9a9e24478a3df144ee8ddfb9e5ff256
ben <75566095+31b4@users.noreply.github.com>
2024-09-20 10:33:43 +0200

    Create main.py

---
+++ 214. Shortest Palindrome/main.py
@@ -0,0 +1,6 @@
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        r = s[::-1]
+        for i in range(len(s) + 1):
+            if s.startswith(r[i:]):
+                return r[:i] + s
