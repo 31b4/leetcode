be00a7ddf25d0f575268ef59c5fdfd0f74f40d66
ben <75566095+31b4@users.noreply.github.com>
2024-10-27 09:56:56 +0100

    Create main.py

---
+++ 1277. Count Square Submatrices with All Ones/main.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        if not matrix or not matrix[0]:
+            return 0
+
+        m, n = len(matrix), len(matrix[0])
+        dp = [[0] * n for _ in range(m)]
+        total_squares = 0
+        
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 1:
+                    if i == 0 or j == 0:
+                        dp[i][j] = 1  # On edges, a single '1' forms a 1x1 square
+                    else:
+                        dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+                    total_squares += dp[i][j]
+
+        return total_squares
